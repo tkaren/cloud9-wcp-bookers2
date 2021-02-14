0941846558534d84d1805cc3955db2ba656abb13
tsunodakaren <k6432286@gmail.com>
2021-02-14 13:03:36 +0000

    [Add] メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-
+  before_action :authenticate_user!
   def new
     @book = Book.new
   end
@@ -7,27 +7,37 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render action: :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
     @user = @book.user
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-      redirect_to book_path(@book.id)
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully"
+    redirect_to book_path(@book.id)
+    else
+    render action: :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @user = User.find(params[:id])
     #@books = @user.books.page(params[:page]).reverse_order
@@ -11,8 +13,12 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully"
+      redirect_to user_path(@user.id)
+    else
+      render action: :edit
+    end
   end
   
   def index
