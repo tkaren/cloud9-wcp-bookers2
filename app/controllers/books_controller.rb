d4adf451f741f36b7dcf02fa00d5acb960e28036
tsunodakaren <k6432286@gmail.com>
2021-02-14 05:51:51 +0000

    [Edit]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,17 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+      redirect_to book_path(@book.id)
   end
 
   def destroy
