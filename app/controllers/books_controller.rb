a27beeb2b08c400a01b8b729167ffd07354b2451
tsunodakaren <k6432286@gmail.com>
2021-02-15 09:56:41 +0000

    [Add] bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only:[:edit, :update, :destroy]
   def new
     @book = Book.new
   end
@@ -12,6 +13,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       @books = Book.all
+      @user = current_user
       render action: :index
     end
   end
@@ -19,11 +21,13 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_new = Book.new
   end
 
   def edit
@@ -46,8 +50,17 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+     unless @book.user == current_user
+     redirect_to books_path
+     end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only:[:update, :edit]
   
   def show
     @user = User.find(params[:id])
@@ -23,12 +24,21 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    @book = Book.new
+    @user = current_user
   end
   
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+     unless @user == current_user
+       redirect_to user_path
+     end
   end
   
 end
